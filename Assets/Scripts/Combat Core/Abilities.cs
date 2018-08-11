3552dd772a355b33c5b3c85de9da3c774228ddd7
Sam Streed <embersteelstreus@gmail.com>
2018-08-11 17:22:28 -0500

    Added Player Dash.

--- Assets/Scripts/Combat Core/Abilities.cs
+++ Assets/Scripts/Combat Core/Abilities.cs
@@ -11,21 +11,72 @@ namespace CombatCore
 
 			repository = new Dictionary<string, Ability> ();
 
-			//player offensive
 			Put (new Ability (
 				"DEBUG",
 				"Does nothing",
 				null,
 				0.1f,
 				0,
-				"DebugAbil")
+				DebugAbil)
+			);
+
+			Put (new Ability (
+				"Dash",
+				"Dashes to a place",
+				null,
+				0.1f,
+				0,
+				PlayerDash)
+			);
+
+			Put (new Ability (
+				"Shoot",
+				"Shoots a bullet",
+				null,
+				0f,
+				0,
+				PlayerShoot)
+			);
+
+			Put (new Ability (
+				"Reflect",
+				"Provides a short period of reflection",
+				null,
+				1f,
+				0,
+				PlayerReflect)
 			);
 		}
 
-		private bool DebugAbil(Entity subject, Vector2 targetPosition, params object[] args)
+		private static bool DebugAbil(Entity subject, Vector3 targetPosition, params object[] args)
 		{
 			Debug.Log ("BANG");
 			return true;
 		}
+
+		#region PLAYER_ABILITIES
+		private static bool PlayerDash(Entity subject, Vector3 targetPosition, params object[] args)
+		{
+			float range = (float)args[0];
+			Debug.Log ("PlayerDash: " + range);
+			Vector3 dir = targetPosition - subject.transform.position;
+			Vector3 targetPos = subject.transform.position + dir.normalized * Mathf.Min (range, dir.magnitude);
+			Controller c = subject.GetComponent<Controller> ();
+			c.StartCoroutine (c.Dashing (targetPos));
+			return true;
+		}
+
+		private static bool PlayerShoot(Entity subject, Vector3 targetPosition, params object[] args)
+		{
+			Debug.Log ("PlayerShoot");
+			return true;
+		}
+
+		private static bool PlayerReflect(Entity subject, Vector3 targetPosition, params object[] args)
+		{
+			Debug.Log ("PlayerReflect");
+			return true;
+		}
+		#endregion
 	}
 }
