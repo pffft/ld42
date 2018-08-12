8988a0655fb84b091a7c4c2ae9055ad2ad0828b7
George Aleksandrovich <georgealeks@hotmail.com>
2018-08-11 23:37:10 -0500

    * Created Projectile infrastructure and simple Boss shooting

---
+++ Assets/Scripts/Boss Core/BossController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CombatCore;
+using UnityEngine;
+
+public class BossController : MonoBehaviour {
+
+    private GameObject player;
+
+    private Rigidbody physbody;
+    private CombatCore.Entity self;
+
+    private void Awake() {
+        player = GameObject.Find("Player");
+
+        physbody = GetComponent<Rigidbody>();
+        self = GetComponent<CombatCore.Entity>();
+	}
+
+	// Use this for initialization
+	void Start () {
+        self.AddAbility(new CombatCore.Ability("shoot", "", null, 1, 1, Shoot1), 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.transform.position;
+
+        if (Input.GetKey(KeyCode.P)) {
+            if (self.GetAbility(2).Use(self, playerPos)) {}
+        }
+	}
+
+    private bool Shoot1(Entity subject, Vector3 targetPosition, params object[] args) {
+        ProjectileManager.spawn1(transform.position, player.transform.position);
+        return true;
+    }
+}
---
+++ Assets/Scripts/Boss Core/ProjectileManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileManager {
+
+    public static void spawn1(Vector3 spawn, Vector3 target)
+    {
+        Projectile.Create(spawn, Quaternion.FromToRotation(Vector3.right, target), 1f, 10f);
+    }
+
+}
