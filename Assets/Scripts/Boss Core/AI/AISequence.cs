a909e2747c7316af490443b36da404accf5a2536
George Aleksandrovich <georgealeks@hotmail.com>
2018-08-13 01:51:01 -0500

    * Added ability to create an AISequence from n AIEvents * Expanded demo to to show off new features

--- Assets/Scripts/Boss Core/AI/AISequence.cs
+++ Assets/Scripts/Boss Core/AI/AISequence.cs
@@ -9,9 +9,32 @@ public class AISequence
 
     private static Dictionary<string, AISequence> sequenceDictionary = new Dictionary<string, AISequence>();
 
+    private AISequence() {}
+
     public AISequence(params AIEvent[] events)
     {
         this.events = events;
+    }
+
+    public AISequence(params object[] events) {
+        List<AIEvent> eventsList = new List<AIEvent>();
+        foreach (object o in events) {
+            if (o is AIEvent) {
+                eventsList.Add((AIEvent)o);
+            } else if (o is AISequence) {
+                eventsList.AddRange(((AISequence)o).events);
+            }
+        }
+    }
+
+    public static AISequence Repeat(AIEvent iEvent, int times) {
+        AIEvent[] events = new AIEvent[times];
+        for (int i = 0; i < times; i++) {
+            events[i] = iEvent;
+        }
+        AISequence sequence = new AISequence();
+        sequence.events = events;
+        return sequence;
     }
 
     public static void AddSequence(string name, AISequence sequence) {
