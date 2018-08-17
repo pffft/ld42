1ffab069040d05888aee864a373f9d579880f8c5
George Aleksandrovich <georgealeks@hotmail.com>
2018-08-17 02:05:06 -0500

    * Refactored AIEvent and AISequence to a Builder pattern * Moved BossController code into static AISequence dictionary * Worked on refactoring BossController methods and adding more robust sweeping abilities

--- Assets/Scripts/Boss Core/AI/SequenceDictionary.cs
+++ Assets/Scripts/Boss Core/AI/SequenceDictionary.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using static BossController;
+using Projectiles;
+
 // TODO: move all boss controller sequences in here
 // make a JSON parser to make this job easier?
 //
@@ -13,15 +16,231 @@ namespace AI
     public partial class AISequence
     {
 
-        private static BossController bc = BossController.GetInstance();
+        #region Building Block Sequences
 
-        public static AISequence SHOOT3_WAVE3 = new AISequence(
-            new AIEvent(0.5f, bc.Teleport()),
-            new AIEvent(0f, bc.ShootWave(50, 360f, 0f)),
-            new AIEvent(0.1f, bc.Shoot3()).Times(20),
-            new AIEvent(0f, bc.ShootWave(50, 360f, 0f)),
-            new AIEvent(0.1f, bc.Shoot3()).Times(20),
-            new AIEvent(0f, bc.ShootWave(50, 360f, 0f))
+        public static AISequence SHOOT_360 = new AISequence(
+            ShootWave(50, 360f, 0f)
         );
+
+        /*
+         * Shoots 2 90 waves as one block, encouraging dodging through them.
+         */
+        public static AISequence SHOOT_2_WAVES = new AISequence(
+            Teleport(),
+            ShootWave(25, 90f, 2.5f, speed: Speed.VERY_FAST),
+            ShootWave(25, 90f, -2.5f, speed: Speed.VERY_FAST)
+            .Wait(1f)
+        );
+
+        /*
+         * Shoots a 165 wave with a 45 degree gap in the middle.
+         */
+        public static AISequence SHOOT_WAVE_MIDDLE_GAP = new AISequence(
+            ShootWave(25, 60f, -45f, speed: Speed.VERY_FAST),
+            ShootWave(25, 60f, 45f, speed: Speed.VERY_FAST)
+        );
+
+        /*
+         * Fires a homing projectile, then strafes 5 degrees.
+         */
+        public static AISequence HOMING_STRAFE_5 = new AISequence(
+            Strafe(true, 5f, 100),
+            Shoot1(Type.HOMING, Size.MEDIUM)
+        );
+
+        /*
+         * Fires a homing projectile, then strafes 10 degrees.
+         */
+        public static AISequence HOMING_STRAFE_10 = new AISequence(
+            Strafe(true, 10f, 50),
+            Shoot1(Type.HOMING, Size.MEDIUM)
+        );
+
+        /*
+         * Fires a homing projectile, then strafes 15 degrees.
+         */
+        public static AISequence HOMING_STRAFE_15 = new AISequence(
+            Strafe(true, 15f, 30),
+            Shoot1(Type.HOMING, Size.MEDIUM)
+        );
+
+        /*
+         * Fires a homing projectile, then strafes 65 degrees.
+         */
+        public static AISequence HOMING_STRAFE_65 = new AISequence(
+            Strafe(true, 65f, 50),
+            Shoot1(Type.HOMING, Size.MEDIUM)
+        );
+
+        /*
+         * Fires a line at the player, then strafes 60 degrees.
+         */
+        public static AISequence LINE_STRAFE_60 = new AISequence(
+            ShootLine(50, 75f, Speed.SNIPE).Wait(0.2f),
+            Strafe(true, 60f, 50)
+        );
+
+        /*
+         * Shoots a medium-slow 360, then strafes 60 degrees.
+         */
+        public static AISequence SLOW_WAVE_CIRCLE = new AISequence(
+            ShootWave(75, 360f, 0f, speed: Speed.MEDIUM_SLOW, size: Size.LARGE),
+            Strafe(true, 60f, 50).Wait(0.5f)
+        );
+
+        /* 
+         * Shoots a slow 360 wave, strafes 30, fires a line at center, and strafes 30 again.
+         * Total strafe: 60 degrees.
+         */
+        /*
+        public static AISequence LINE_CIRCLE_STRAFE_60 = new AISequence(
+            ShootWave(75, 360f, 0f, speed: Speed.SLOW, size: Size.LARGE)
+            .Wait(0.1f)
+            .Then(
+                Strafe(true, 30f, 50))
+            .Wait(0.3f)
+            .Then(
+                ShootLine(50, 100f, Speed.VERY_FAST, Vector3.zero))
+            .Wait(0.2f)
+            .Then(
+                Strafe(true, 30f, 50))
+        );
+        */
+
+        public static AISequence LINE_CIRCLE_STRAFE_60 = new AISequence(
+            ShootWave(75, 360f, 0f, speed: Speed.SLOW, size: Size.LARGE)
+            .Wait(0.1f),
+            Strafe(true, 30f, 50)
+            .Wait(0.3f),
+            ShootLine(50, 100f, Speed.VERY_FAST, Vector3.zero)
+            .Wait(0.2f),
+            Strafe(true, 30f, 50)
+        );
+
+        #endregion
+
+        #region Full Moveset Sequences
+        /*
+         * 40 basic bullets, with a 360 wave at the start, middle, and end.
+         */
+        public static AISequence SHOOT3_WAVE3 = new AISequence(
+            Teleport().Wait(0.5f),
+            SHOOT_360,
+            Shoot3().Wait(0.1f).Times(20),
+            SHOOT_360,
+            Shoot3().Wait(0.1f).Times(20),
+            SHOOT_360
+        );
+
+        public static AISequence HEX_CURVE_INTRO = new AISequence(
+            ShootHexCurve(true),
+            ShootWave(50, 360f, 0).Wait(2.5f),
+            ShootHexCurve(false),
+            ShootWave(50, 360f, 0).Wait(2.5f),
+            ShootHexCurve(true),
+            SHOOT_360,
+            ShootHexCurve(false).Wait(1f),
+            SHOOT_360.Wait(1f),
+            SHOOT_360.Wait(1.5f),
+            Teleport().Wait(0.5f),
+            HOMING_STRAFE_10.Times(12),
+            SHOOT_2_WAVES.Times(3)
+        );
+
+        public static AISequence BIG_HOMING_STRAFE = new AISequence(
+            CameraMove(false, new Vector3(0, 17.5f, -35f)).Wait(1f),
+            Teleport(NORTH_FAR).Wait(1f),
+            HOMING_STRAFE_65.Times(10),
+            Teleport(NORTH_FAR).Wait(1f),
+            HOMING_STRAFE_15.Times(15),
+            CameraMove(true).Wait(2f)
+        );
+
+        public static AISequence DOUBLE_HEX_CURVE = new AISequence(
+            Teleport(CENTER).Wait(1.5f),
+            ShootHexCurve(true, 0f),
+            ShootWave(50, 360, 0f).Wait(0.5f),
+            ShootHexCurve(true, 30f).Wait(0.5f),
+            SHOOT_360.Wait(1f),
+            SHOOT_360.Wait(1f)
+        );
+
+        public static AISequence HOMING_STRAFE_WAVE_SHOOT = new AISequence(
+            Teleport().Wait(0.2f),
+            HOMING_STRAFE_15.Times(15),//.Wait(0.3f) // This is very hard; adding wait is reasonable
+            SHOOT_2_WAVES.Times(2)
+        );
+
+        /*
+         * A really intricate pattern. 6 projectiles that explode into 6 more projectiles,
+         * repeated twice to forme a lattice.
+         */
+        public static AISequence DEATH_HEX = new AISequence(
+            Teleport(CENTER).Wait(0.5f),
+            ShootDeathHex(2f).Wait(1f),
+            ShootDeathHex(1f).Wait(5f)
+        );
+
+        /*
+         * Fires six slow circles around the arena in a circular pattern.
+         * Then repeats twice, with lines appearing on the left and right sides.
+         */
+        public static AISequence WAVE_CIRCLE = new AISequence(
+            Teleport(WEST_MED),
+            SLOW_WAVE_CIRCLE.Times(6),
+            SLOW_WAVE_CIRCLE.Times(3),
+            ShootLine(50, 75f, Speed.MEDIUM_SLOW, Vector3.left),
+            SLOW_WAVE_CIRCLE.Times(3),
+            ShootLine(50, 75f, Speed.MEDIUM_SLOW, Vector3.right),
+            SLOW_WAVE_CIRCLE.Times(3),
+            ShootLine(50, 75f, Speed.MEDIUM_SLOW, Vector3.left),
+            SLOW_WAVE_CIRCLE.Times(3),
+            ShootLine(50, 75f, Speed.MEDIUM_SLOW, Vector3.right)
+        );
+
+        public static AISequence JUMP_ROPE_FAST = new AISequence(
+            CameraMove(false, new Vector3(0, 17.5f, -35)).Wait(1f),
+            Teleport(WEST_FAR, 200),
+            ShootLine(50, 100f, Speed.SNIPE),
+            Teleport(EAST_FAR, 200),
+            ShootLine(50, 100f, Speed.SNIPE),
+            Teleport(WEST_FAR, 200),
+            ShootLine(50, 100f, Speed.SNIPE),
+            Teleport(EAST_FAR, 200),
+            ShootLine(50, 100f, Speed.SNIPE),
+            CameraMove(true)
+        );
+
+        public static AISequence DOUBLE_HEX_CURVE_HARD = new AISequence(
+            Teleport(CENTER).Wait(1f),
+            ShootHexCurve(true, 0f).Wait(0.5f),
+            ShootHexCurve(true, 30f).Wait(0.5f),
+            SHOOT3_WAVE3,
+            Teleport(CENTER),
+            ShootHexCurve(false, 0f),
+            ShootHexCurve(false, 30f),
+            // This homing might be too hard; especially with this amount of 360s.
+            Shoot3(Type.HOMING, Size.MEDIUM).Wait(0.1f).Times(10),
+            SHOOT_360,
+            Shoot3(Type.HOMING, Size.MEDIUM).Wait(0.1f).Times(5),
+            SHOOT_360,
+            Shoot3(Type.HOMING, Size.MEDIUM).Wait(0.1f).Times(5),
+            SHOOT_360.Wait(0.5f),
+            SHOOT_360.Wait(0.5f)
+        );
+
+        public static AISequence test = new AISequence(
+            LINE_CIRCLE_STRAFE_60.Times(6)
+        );
+
+        public static AISequence JUMP_ROPE_SLOW_CIRCLES = new AISequence(
+            Teleport(WEST_FAR),
+            LINE_STRAFE_60.Times(6),
+            LINE_CIRCLE_STRAFE_60.Times(6)
+        );
+
+
+
+        #endregion
     }
 }
