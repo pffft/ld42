4a7bbd09c0be8b0d2dc7b762ed0da794ea579091
aleksandrovich <aleksandrovich@oath.com>
2018-09-18 19:09:33 -0500

    Added missing files

---
+++ Assets/Scripts/Boss Core/AI/PhaseDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using static AI.AISequence;
+
+namespace AI
+{
+    public partial class AIPhase
+    {
+
+        /*
+         * 
+        eventQueue.AddSequence(AISequence.SHOOT3_WAVE3);
+        eventQueue.AddSequence(AISequence.SHOOT3_WAVE3);
+
+        eventQueue.AddSequence(AISequence.SHOOT_2_WAVES.Times(5));
+
+        eventQueue.AddSequence(AISequence.HEX_CURVE_INTRO);
+
+        eventQueue.AddSequence(AISequence.BIG_HOMING_STRAFE);
+
+        eventQueue.AddSequence(AISequence.DOUBLE_HEX_CURVE);
+
+        eventQueue.AddSequence(AISequence.HOMING_STRAFE_WAVE_SHOOT.Times(2));
+
+        eventQueue.AddSequence(AISequence.DEATH_HEX);
+
+        eventQueue.AddSequence(AISequence.WAVE_CIRCLE);
+
+        eventQueue.AddSequence(AISequence.JUMP_ROPE_FAST);
+
+        eventQueue.AddSequence(AISequence.DOUBLE_HEX_CURVE_HARD);
+
+        eventQueue.AddSequence(AISequence.JUMP_ROPE_SLOW_CIRCLES);
+        */
+
+        public static AIPhase PHASE1 = new AIPhase()
+            .AddSequence(10, SHOOT3_WAVE3)
+            .AddSequence(10, SHOOT_2_WAVES.Times(5))
+            .AddSequence(10, HEX_CURVE_INTRO)
+            .AddSequence(10, BIG_HOMING_STRAFE)
+            .AddSequence(10, DOUBLE_HEX_CURVE)
+            .AddSequence(10, HOMING_STRAFE_WAVE_SHOOT.Times(2))
+            .AddSequence(10, DEATH_HEX)
+            .AddSequence(10, WAVE_CIRCLE)
+            .AddScriptedSequence(5, JUMP_ROPE_FAST);
+    }
+}
