233779e30741e22c94ff8ef69fce685747689915
Sam Streed <embersteelstreus@gmail.com>
2018-09-30 15:16:35 -0500

    Added basic UI stuff.

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,39 @@
+﻿using CombatCore;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+	[SerializeField]
+	private Entity player, shield;
+
+	[SerializeField]
+	private Image shieldIndicator;
+
+	[SerializeField]
+	private Image dashIndicator;
+
+	[SerializeField]
+	private Vector3 followOffset;
+
+	public void Update()
+	{
+		if (shield != null && shieldIndicator != null)
+		{
+			shieldIndicator.fillAmount = shield.ShieldPerc;
+		}
+	}
+
+	public void LateUpdate()
+	{
+		if (player != null)
+		{
+			transform.position = player.transform.position + followOffset;
+
+			if (dashIndicator != null)
+			{
+				dashIndicator.transform.position = player.GetComponent<Controller> ().GetDashTargetPoint () + Vector3.up * 0.01f;
+			}
+		}
+	}
+}
