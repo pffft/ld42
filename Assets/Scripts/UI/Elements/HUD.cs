21cdc8181cde1341d544f9ae712fc3e7c7434b90
Sam Streed <embersteelstreus@gmail.com>
2018-10-18 19:41:21 -0500

    Added 'arrow' UI to HUD that points to thrown shield.

--- Assets/Scripts/UI/Elements/HUD.cs
+++ Assets/Scripts/UI/Elements/HUD.cs
@@ -11,8 +11,12 @@ public class HUD : MonoBehaviour
 	[SerializeField]	
 	private Entity shield = null;
 
+	[Header("Shield")]
 	[SerializeField]
 	private Image shieldIndicator = null;
+
+	[SerializeField]
+	private Image shieldPointer;
 
 	[SerializeField]
 	private Color[] shieldIndicatorColors = new Color[2];
@@ -25,8 +29,17 @@ public class HUD : MonoBehaviour
 		set { unavailableShieldIndicatorColor = value; }
 	}
 
+	[SerializeField]
+	private Color shieldPointerColor;
+	public Color ShieldPointerColor
+	{
+		get { return shieldPointerColor; }
+		set { shieldPointerColor = value; }
+	}
+
 	public bool shieldAvailable = true;
 
+	[Header ("Dash")]
 	[SerializeField]
 	private Image dashIndicator = null;
 
@@ -51,9 +64,27 @@ public class HUD : MonoBehaviour
 		if (shieldIndicator != null && shield != null)
 		{
 			if (shieldAvailable)
+			{
 				shieldIndicator.color = Color.Lerp (shieldIndicatorColors[1], shieldIndicatorColors[0], shield.ShieldPerc);
+				if (shieldPointer != null)
+				{
+					shieldPointer.color = Color.clear;
+				}
+			}
 			else
+			{
 				shieldIndicator.color = unavailableShieldIndicatorColor;
+				if (shieldPointer != null)
+				{
+					shieldPointer.color = shieldPointerColor;
+					shieldPointer.fillAmount = 1f - (5f / Vector3.Distance (transform.position, GameManager.ThrownShield.transform.position));
+					Quaternion q = Quaternion.LookRotation ((GameManager.ThrownShield.transform.position - transform.position).normalized, Vector3.forward);
+					shieldPointer.transform.rotation = Quaternion.Euler (
+						shieldPointer.transform.rotation.eulerAngles.x, 
+						q.eulerAngles.y, 
+						shieldPointer.transform.rotation.eulerAngles.z);
+				}
+			}
 
 			shieldIndicator.fillAmount = shield.ShieldPerc;
 		}
