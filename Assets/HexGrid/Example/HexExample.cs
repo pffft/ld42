a892adff817140edfb0ce8b64d1fcec53b774220
Sam Streed <embersteelstreus@gmail.com>
2020-03-07 01:01:44 -0600

    Working on 2d plane conversion tool; not quite there yet

---
+++ Assets/HexGrid/Example/HexExample.cs
@@ -0,0 +1,30 @@
+﻿using Hex;
+using UnityEngine;
+
+public class HexExample : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tilePrefab = null;
+
+    [SerializeField]
+    private int radius = 10;
+
+    [SerializeField]
+    private Vector3 planeNormal = Vector3.up;
+
+    private void Start()
+    {
+        HexGrid grid = new HexGrid(1f);
+        GridPlane plane = new GridPlane(Vector3.back);
+        RangeHex area = new RangeHex(VectorHex.zero, radius);
+
+        Debug.Log($"{Vector2.one} -> {plane.Normal} = {plane[Vector2.one]}");
+
+        //Vector3 prefabPostion;
+        //foreach (VectorHex hexPos in area)
+        //{
+        //    prefabPostion = plane.Get3dPositionAt(grid.HexToCart(hexPos));
+        //    Instantiate(tilePrefab?? new GameObject(), prefabPostion, Quaternion.identity);
+        //}
+    }
+}
