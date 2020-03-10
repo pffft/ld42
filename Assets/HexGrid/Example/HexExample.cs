8b9498c005861a012cbb814252120fb6e6dbc79f
Sam Streed <embersteelstreus@gmail.com>
2020-03-09 19:55:19 -0500

    Space conversion stuff done.

--- Assets/HexGrid/Example/HexExample.cs
+++ Assets/HexGrid/Example/HexExample.cs
@@ -1,4 +1,5 @@
 ﻿using Hex;
+using Hex.Converters;
 using UnityEngine;
 
 public class HexExample : MonoBehaviour
@@ -9,22 +10,17 @@ public class HexExample : MonoBehaviour
     [SerializeField]
     private int radius = 10;
 
-    [SerializeField]
-    private Vector3 planeNormal = Vector3.up;
-
     private void Start()
     {
-        HexGrid grid = new HexGrid(1f);
-        GridPlane plane = new GridPlane(Vector3.back);
+        HexGrid grid = new HexGrid();
         RangeHex area = new RangeHex(VectorHex.zero, radius);
+        HexToCartXZ xzSpace = new HexToCartXZ(grid);
 
-        Debug.Log($"{Vector2.one} -> {plane.Normal} = {plane[Vector2.one]}");
-
-        //Vector3 prefabPostion;
-        //foreach (VectorHex hexPos in area)
-        //{
-        //    prefabPostion = plane.Get3dPositionAt(grid.HexToCart(hexPos));
-        //    Instantiate(tilePrefab?? new GameObject(), prefabPostion, Quaternion.identity);
-        //}
+        Vector3 prefabPostion;
+        foreach (VectorHex hexPos in area)
+        {
+            prefabPostion = xzSpace.convertTo(hexPos);
+            Instantiate(tilePrefab ?? new GameObject(), prefabPostion, Quaternion.identity);
+        }
     }
 }
---
+++ Assets/HexGrid/Scripts/Conversion/HexToCartXZ.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Hex.Converters
+{
+    public class HexToCartXZ : ISpaceConverter<VectorHex, Vector3>
+    {
+        public HexGrid Grid { get; }
+        private CartXYToXZ cartXYToXZ;
+
+        public HexToCartXZ(HexGrid grid)
+        {
+            Grid = grid;
+            cartXYToXZ = new CartXYToXZ();
+        }
+
+        public VectorHex convertFrom(Vector3 position) => Grid.CartToHex(cartXYToXZ.convertFrom(position));
+
+        public Vector3 convertTo(VectorHex position) => cartXYToXZ.convertTo(Grid.HexToCart(position));
+    }
+}
