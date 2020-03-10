35763d4b9a21967437b30a794be9ffab09406334
Sam Streed <embersteelstreus@gmail.com>
2020-03-09 19:55:19 -0500

    Space conversion stuff done.

---
+++ Assets/HexGrid/Scripts/Conversion/CartXYToXZ.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Hex.Converters
+{
+    public class CartXYToXZ : ISpaceConverter<Vector2, Vector3>
+    {
+        public Vector2 convertFrom(Vector3 position) => new Vector2(position.x, position.z);
+
+        public Vector3 convertTo(Vector2 position) => new Vector3(position.x, 0f, position.y);
+    }
+}
